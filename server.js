e1d4c797cb63b5a585e3e567a4fe3fb9f5242b5f
mktexan <mkcoarng@gmail.com>
2020-12-11 15:52:26 -0700

    removed library

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const constants = require('./constants/constants')
 require('dotenv').config()
 
 //*** Modules ***
-const bodyParser = require('body-parser')
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
